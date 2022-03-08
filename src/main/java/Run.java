c0cee912eaeda648cef10984cb9330d2532ce65d
NickDue <ndja19@student.aau.dk>
2022-03-08 17:10:44 +0100

    Close to finishing, just need some tests

---
+++ src/main/java/Run.java
@@ -0,0 +1,20 @@
+
+public class Run {
+    private static double time, money;
+    private static Model model;
+
+    public static void main(String[] args) {
+        if(args.length < 3){
+            System.out.println("""
+                    Not enough arguments, need at least three\s
+                    1st Argument: Time measures in minutes\s
+                    2nd Argument: Money cost\s
+                    3rd Argument: Model identifier""");
+        }
+        CostConverter converter = new CostConverter(args);
+        if (args.length == 3)
+            System.out.println((int)Math.ceil(converter.ConvertCost(false)));
+        else
+            System.out.println((int)Math.ceil(converter.ConvertCost(true)));
+    }
+}
